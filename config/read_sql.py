da1bebaa087e62035e8fcb1f6fe2a769f1d0afa0
97612336 <xkjskk@163.com>
2018-07-01 23:54:33 +0800

    增加文件

--- config/read_sql.py
+++ config/read_sql.py
@@ -1,13 +1,13 @@
 import json
 import os
 
-user_home = os.environ.get("HOME")
+#获取MySQL的配置文件
+def get_mysql_conf():
+    user_home = os.environ.get("HOME")
+    conf_base_path = user_home + "/conf"
+    mysql_conf_path = conf_base_path + "/mysql.conf"
+    with open(mysql_conf_path, "r") as f1:
+        res_str = f1.read()
+    mysql_dict = json.loads(res_str)
+    return mysql_dict
 
-conf_base_path = user_home + "/conf"
-
-mysql_conf_path = conf_base_path + "/mysql.conf"
-
-with open(mysql_conf_path, "r") as f1:
-    res_str = f1.read()
-
-mysql_dict = json.loads(res_str)
