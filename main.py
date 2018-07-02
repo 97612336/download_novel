addb22ce28dcdf3562431e0950c2856ed29d76b8
wangkun <wangkun@autoforce.net>
2018-07-02 11:30:51 +0800

    测试爬取环境

--- main.py
+++ main.py
@@ -7,3 +7,21 @@
 
 # 开始爬取的url
 # http://www.biquge.com.tw/1_1111/
+import chardet
+import requests
+
+url = r"http://www.biquge.com.tw/1_1111/"
+
+r = requests.get(url)
+
+bytes_html = r.content
+
+print(bytes_html)
+
+res = chardet.detect(bytes_html)
+
+encoding = res.get('encoding')
+
+str_html = bytes_html.decode(encoding)
+
+print(str_html)
