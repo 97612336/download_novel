c88b573f69422fa2ef6c946d70ef869c9e753008
wangkun <wangkun@autoforce.net>
2018-07-19 09:48:22 +0800

    增加header

--- get_html/get_html_text.py
+++ get_html/get_html_text.py
@@ -8,13 +8,6 @@ import requests
 def get_html_text(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Connection": "keep-alive",
-        "Cookie": "__cdnuid=aba66920f28c86c961bbfb641ffe90d4; "
-                  "PHPSESSID=7qfmgl8att4h60fpqf56kq6pv6; asdplan8718=1; dsa=1; asdplan9032=1",
-        "Host": "http://www.biquge.com.tw",
-        "Referer": "http://www.biquge.com.tw"
     }
     res = requests.get(url, headers=headers)
     html_bytes = res.content
